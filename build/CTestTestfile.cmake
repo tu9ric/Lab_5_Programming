43b78199c1e8be04b3774985aa2a0c795c4cb1c8
ark23 <ark23@tpu.ru>
2024-05-09 13:06:11 +0700

    commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/artem/OneDrive/Desktop/Study/Programming/LAB_5
+# Build directory: C:/Users/artem/OneDrive/Desktop/Study/Programming/LAB_5/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
